812aabcace6a7c750f954fd16ab435d8a17ae7a3
Ahmed Ali <aali@cinnova.com>
2024-01-19 15:36:31 +0500

    lal

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
-const http2 = require('http2');
-const fs = require('fs');
+var http = require('http'); // 1 - Import Node.js core module
+const exec = require('child_process').exec;
+var server = http.createServer(function (req, res) {   // 2 - creating server
+    if (req.url == '/') { //check the URL of the current request
+        const child = exec('sh my_script.sh',
+            (error, stdout, stderr) => {
+                console.log(stdout);
+                console.log(`stderr: ${stderr}`);
+                if (error !== null) {
+                    console.log(`exec error: ${error}`);
+                }
+                // set response header|
+                res.writeHead(200, { 'Content-Type': 'text/html' });
 
-const server = http2.createSecureServer({
-    key: fs.readFileSync('./private.pem'),
-    cert: fs.readFileSync('./cert.pem'),
+                // set response content    
+                res.write(stdout);
+                res.end();
+
+            });
+
+    }
+    if(req.url === '/abc'){
+        
+    }
+    //handle incomming requests here..
+
 });
 
-server.on("stream", (stream, headers) => {
-    console.log(stream.id);
-    stream.respond({
-        "content-type": "application/json",
-        "status": 200
-    })
+server.listen(8001); //3 - listen for any incoming requests
 
-    stream.end(JSON.stringify({
-        "user": "Hussein",
-        "id": 823
-    }))
-})
+console.log('Node.js web server at port 8001 is running..')
 
-server.on('error', (error) => {
-    console.error('Server error:', error);
-});
-
-server.listen(3000, () => {
-    console.log('Server is listening on port 3000');
-});
