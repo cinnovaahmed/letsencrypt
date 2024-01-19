efaaef0004a48b5542ea6c84b596995d28d54319
Ahmed Ali <aali@cinnova.com>
2024-01-19 18:52:33 +0500

    a

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http'); // 1 - Import Node.js core module
 const exec = require('child_process').exec;
 var server = http.createServer(function (req, res) {   // 2 - creating server
     if (req.url == '/') { //check the URL of the current request
-        const child = exec('sh my_script.sh',
+        const child = exec('./my_script.sh ',
             (error, stdout, stderr) => {
                 console.log(stdout);
                 console.log(`stderr: ${stderr}`);
@@ -13,7 +13,8 @@ var server = http.createServer(function (req, res) {   // 2 - creating server
                 res.writeHead(200, { 'Content-Type': 'text/html' });
 
                 // set response content    
-                res.write(stdout);
+                let str = stdout.trim().split(' ')
+                res.write(JSON.stringify(str));
                 res.end();
 
             });
