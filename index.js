044103c906f8a2eaaf3bd294d376f4fd6b7e62f8
Ahmed Ali <aali@cinnova.com>
2024-01-23 17:33:09 +0500

    a

--- index.js
+++ index.js
@@ -1,62 +1,37 @@
 var http = require('http'); // 1 - Import Node.js core module
 const exec = require('child_process').exec;
-var server = http.createServer(function (req, res) {   // 2 - creating server
-    if (req.url == '/') { //check the URL of the current request
-        console.log(req.body, 'abc')
-        try {
-            const child = exec('./my_script.sh "*.lab.yourdomain.com" "your-email@example.com"',
-                (error, stdout, stderr) => {
-                    console.log('Command:', error);
-                    console.log('stdout:', stdout);
-                    console.log('stderr:', stderr);
+var server = http.createServer(function (req, res) {
 
-                    if (error !== null) {
-                        console.log(`exec error: ${error}`);
-                    }
-                    res.writeHead(200, { 'Content-Type': 'application/json' });
-                    res.end(JSON.stringify({ stdout, stderr, error }));
+    bodyParser.json()(req, res, async function () {
+        if (req.url == '/') { //check the URL of the current request
+            console.log(req.body, 'abc')
+            try {
+                const child = exec('./my_script.sh "*.lab.yourdomain.com" "your-email@example.com"',
+                    (error, stdout, stderr) => {
+                        console.log('Command:', error);
+                        console.log('stdout:', stdout);
+                        console.log('stderr:', stderr);
 
-                });
+                        if (error !== null) {
+                            console.log(`exec error: ${error}`);
+                        }
+                        res.writeHead(200, { 'Content-Type': 'application/json' });
+                        res.end(JSON.stringify({ stdout, stderr, error }));
 
-        } catch (error) {
-            res.writeHead(200, { 'Content-Type': 'application/json' });
+                    });
 
-            // set response content    
+            } catch (error) {
+                res.writeHead(200, { 'Content-Type': 'application/json' });
 
-            res.write(error);
-            res.end();
+                // set response content    
+
+                res.write(error);
+                res.end();
+            }
+
         }
+    })
 
-    }
-    if (req.url === '/abc') {
-        const greenlock = require('greenlock');
-
-        const lex = greenlock.create({
-            // Replace with your domain
-            servername: 'sysmon.lab.unifiedeverything.com',
-            challenges: {
-                'dns-01': require('greenlock-challenge-dns-provider').create(),
-            },
-        });
-
-        lex.check({
-            domains: ['*.lab.unifiedeverything.com'],
-        }).then(() => {
-            // Access the TXT data
-            const txtData = lex.challenges['dns-01'].get({
-                identifier: 'lab.unifiedeverything.com',
-                dnsChallenge: {
-                    recordName: '_acme-challenge.lab.unifiedeverything.com',
-                },
-            });
-
-            console.log('TXT Data:', txtData);
-        }).catch((err) => {
-            console.error('Error:', err);
-        });
-
-    }
-    //handle incomming requests here..
 
 });
 
