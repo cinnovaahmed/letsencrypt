64c932b7847dd2c65a5e1db81354f649aa3477e4
Ahmed Ali <aali@cinnova.com>
2024-01-30 16:11:10 +0500

    a

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 var http = require('http'); // 1 - Import Node.js core module
 const exec = require('child_process').exec;
 const bodyParser = require('body-parser');
-const { extractDomains } = require('./utility')
 
 var server = http.createServer(function (req, res) {
 
     bodyParser.json()(req, res, async function () {
         if (req.url == '/') { //check the URL of the current request
-            
+            console.log(req.body, 'abc')
             const body = req.body;
             try {
-                let certDomainsStr = extractDomains(body);
-                console.log(certDomainsStr)
-
-                const child = exec(`./my_script.sh "${certDomainsStr}" "${req.body.email}"`,
+                // let certDomainsStr = extractDomains(body);
+                // console.log(certDomainsStr)
+                const child = exec(`./my_script.sh "*.${req.body.portalRoot}" "${req.body.email}"`,
                     (error, stdout, stderr) => {
                         console.log('Command:', error);
                         console.log('stdout:', stdout);
