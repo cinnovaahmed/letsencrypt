6c3772a7d4a2a490d148d829991c33352a5d07d2
Ahmed Ali <aali@cinnova.com>
2024-02-20 15:27:56 +0500

    a

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var http = require('http'); // 1 - Import Node.js core module
 const https = require('https'); // Import Node.js core module for https
-const exec = require('child_process').exec;
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec);
+
 const bodyParser = require('body-parser');
 const { extractCertbotInfo, extractDomains } = require('./utility')
 const fs = require('fs');
@@ -13,159 +15,228 @@ const sslOptions = {
 var server = https.createServer(sslOptions, function (req, res) {
 
     bodyParser.json()(req, res, async function () {
+        // if (req.url == '/') { //check the URL of the current request
+        //     console.log(req.body, 'abc')
+        //     const body = req.body;
+        //     try {
+        //         // let certDomainsStr = extractDomains(body);
+        //         // console.log(certDomainsStr)
+        //         const child = exec(`./my_script.sh "*.${req.body.portalRoot}" "${req.body.email}"`,
+        //             (error, stdout, stderr) => {
+        //                 console.log('Command:', error);
+        //                 console.log('stdout:', stdout);
+        //                 console.log('stderr:', stderr);
+
+        //                 if (error !== null) {
+        //                     console.log(`exec error: ${error}`);
+        //                 }
+        //                 res.writeHead(200, { 'Content-Type': 'application/json' });
+        //                 res.end(JSON.stringify({ stdout, stderr, error }));
+
+        //             });
+
+        //     } catch (error) {
+        //         res.writeHead(500, { 'Content-Type': 'application/json' });
+
+        //         // set response content    
+
+        //         res.write(error);
+        //         res.end();
+        //     }
+
+        // }
+        // if (req.url == '/generate-certs') { //check the URL of the current request
+        //     console.log(req.body, 'abc')
+        //     try {
+        //         const child = exec(`./generate_certs.sh "*.${req.body.portalRoot}" "${req.body.email}"`,
+        //             (error, stdout, stderr) => {
+
+        //                 console.log('Command:', error);
+        //                 console.log('stdout:', stdout);
+        //                 console.log('stderr:', stderr);
+        //                 if (stdout.includes('Successfully received certificate')) {
+        //                     const certData = extractCertbotInfo(stdout)
+        //                     const path = certData.certPath.split('/');
+        //                     path.pop();
+        //                     let domainCert = path.pop();
+        //                     const dirPath = path.join('/');
+        //                     console.log(domainCert);
+
+        //                     const child = exec(`./copy_script.sh "${domainCert}"`,
+        //                         (copyError, copyStdout, copyStderr) => {
+
+        //                             console.log('Command:', copyError);
+        //                             console.log('stdout:', copyStdout);
+        //                             console.log('stderr:', copyStderr);
+
+
+
+        //                             if (copyError !== null) {
+        //                                 console.log(`exec copyError: ${copyError}`);
+        //                             }
+        //                             res.writeHead(200, { 'Content-Type': 'application/json' });
+        //                             res.end(JSON.stringify({ stdout, stderr, error, certData, copyError, copyStderr, copyStdout }));
+
+        //                         });
+
+
+        //                 }
+
+        //             });
+
+        //     } catch (error) {
+        //         res.writeHead(500, { 'Content-Type': 'application/json' });
+
+        //         // set response content    
+
+        //         res.write(error);
+        //         res.end();
+        //     }
+
+        // }
         if (req.url == '/') { //check the URL of the current request
-            console.log(req.body, 'abc')
+            console.log(req.body, 'abc');
             const body = req.body;
             try {
-                // let certDomainsStr = extractDomains(body);
-                // console.log(certDomainsStr)
-                const child = exec(`./my_script.sh "*.${req.body.portalRoot}" "${req.body.email}"`,
-                    (error, stdout, stderr) => {
-                        console.log('Command:', error);
-                        console.log('stdout:', stdout);
-                        console.log('stderr:', stderr);
-
-                        if (error !== null) {
-                            console.log(`exec error: ${error}`);
-                        }
-                        res.writeHead(200, { 'Content-Type': 'application/json' });
-                        res.end(JSON.stringify({ stdout, stderr, error }));
-
-                    });
-
+                const { error, stdout, stderr } = await exec(`./my_script.sh "*.${req.body.portalRoot}" "${req.body.email}"`);
+                console.log('Command:', error);
+                console.log('stdout:', stdout);
+                console.log('stderr:', stderr);
+                if (error !== null) {
+                    console.log(`exec error: ${error}`);
+                }
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ stdout, stderr, error }));
             } catch (error) {
                 res.writeHead(500, { 'Content-Type': 'application/json' });
-
-                // set response content    
-
                 res.write(error);
                 res.end();
             }
-
         }
+
         if (req.url == '/generate-certs') { //check the URL of the current request
-            console.log(req.body, 'abc')
+            console.log(req.body, 'abc');
             try {
-                const child = exec(`./generate_certs.sh "*.${req.body.portalRoot}" "${req.body.email}"`,
-                    (error, stdout, stderr) => {
-
-                        console.log('Command:', error);
-                        console.log('stdout:', stdout);
-                        console.log('stderr:', stderr);
-                        if (stdout.includes('Successfully received certificate')) {
-                            const certData = extractCertbotInfo(stdout)
-                            const path = certData.certPath.split('/');
-                            path.pop();
-                            let domainCert = path.pop();
-                            const dirPath = path.join('/');
-                            console.log(domainCert);
-
-                            const child = exec(`./copy_script.sh "${domainCert}"`,
-                                (copyError, copyStdout, copyStderr) => {
-
-                                    console.log('Command:', copyError);
-                                    console.log('stdout:', copyStdout);
-                                    console.log('stderr:', copyStderr);
-
-
-
-                                    if (copyError !== null) {
-                                        console.log(`exec copyError: ${copyError}`);
-                                    }
-                                    res.writeHead(200, { 'Content-Type': 'application/json' });
-                                    res.end(JSON.stringify({ stdout, stderr, error, certData, copyError, copyStderr, copyStdout }));
-
-                                });
-
-
-                        }
-
-                    });
-
+                const { error, stdout, stderr } = await exec(`./generate_certs.sh "*.${req.body.portalRoot}" "${req.body.email}"`);
+                console.log('Command:', error);
+                console.log('stdout:', stdout);
+                console.log('stderr:', stderr);
+                if (stdout.includes('Successfully received certificate')) {
+                    const certData = extractCertbotInfo(stdout);
+                    const path = certData.certPath.split('/');
+                    path.pop();
+                    let domainCert = path.pop();
+                    const dirPath = path.join('/');
+                    console.log(domainCert);
+                    const { copyError, copyStdout, copyStderr } = await exec(`./copy_script.sh "${domainCert}"`);
+                    console.log('Command:', copyError);
+                    console.log('stdout:', copyStdout);
+                    console.log('stderr:', copyStderr);
+                    if (copyError !== null) {
+                        console.log(`exec copyError: ${copyError}`);
+                    }
+                    res.writeHead(200, { 'Content-Type': 'application/json' });
+                    res.end(JSON.stringify({ stdout, stderr, error, certData, copyError, copyStderr, copyStdout }));
+                }
             } catch (error) {
                 res.writeHead(500, { 'Content-Type': 'application/json' });
-
-                // set response content    
-
                 res.write(error);
                 res.end();
             }
-
         }
-        if (req.url == '/renew-certs') { //check the URL of the current request
-            console.log(req.body, 'abc')
-            try {
-                const child = exec(`./renew_certs.sh "*.${req.body.portalRoot}" "${req.body.email}"`,
-                    (error, stdout, stderr) => {
+        // if (req.url == '/renew-certs') { //check the URL of the current request
+        //     console.log(req.body, 'abc')
+        //     try {
+        //         const child = exec(`./renew_certs.sh "*.${req.body.portalRoot}" "${req.body.email}"`,
+        //             (error, stdout, stderr) => {
 
-                        console.log('Command:', error);
-                        console.log('stdout:', stdout);
-                        console.log('stderr:', stderr);
-                        if (stdout.includes('Successfully received certificate')) {
-                            const certData = extractCertbotInfo(stdout)
-                            const path = certData.certPath.split('/');
-                            path.pop();
-                            let domainCert = path.pop();
-                            const dirPath = path.join('/');
-                            console.log(domainCert);
+        //                 console.log('Command:', error);
+        //                 console.log('stdout:', stdout);
+        //                 console.log('stderr:', stderr);
+        //                 if (stdout.includes('Successfully received certificate')) {
+        //                     const certData = extractCertbotInfo(stdout)
+        //                     const path = certData.certPath.split('/');
+        //                     path.pop();
+        //                     let domainCert = path.pop();
+        //                     const dirPath = path.join('/');
+        //                     console.log(domainCert);
 
-                            const child = exec(`./renew_copy_script.sh "${domainCert}"`,
-                                (copyError, copyStdout, copyStderr) => {
+        //                     const child = exec(`./renew_copy_script.sh "${domainCert}"`,
+        //                         (copyError, copyStdout, copyStderr) => {
 
-                                    console.log('Command:', copyError);
-                                    console.log('stdout:', copyStdout);
-                                    console.log('stderr:', copyStderr);
-
-
-
-                                    if (copyError !== null) {
-                                        console.log(`exec copyError: ${copyError}`);
-                                    }
-                                    res.writeHead(200, { 'Content-Type': 'application/json' });
-                                    res.end(JSON.stringify({ stdout, stderr, error, certData, copyError, copyStderr, copyStdout }));
-
-                                });
+        //                             console.log('Command:', copyError);
+        //                             console.log('stdout:', copyStdout);
+        //                             console.log('stderr:', copyStderr);
 
 
-                        }
 
-                    });
+        //                             if (copyError !== null) {
+        //                                 console.log(`exec copyError: ${copyError}`);
+        //                             }
+        //                             res.writeHead(200, { 'Content-Type': 'application/json' });
+        //                             res.end(JSON.stringify({ stdout, stderr, error, certData, copyError, copyStderr, copyStdout }));
 
-            } catch (error) {
-                res.writeHead(500, { 'Content-Type': 'application/json' });
+        //                         });
 
-                // set response content    
 
-                res.write(error);
-                res.end();
-            }
+        //                 }
 
-        }
+        //             });
+
+        //     } catch (error) {
+        //         res.writeHead(500, { 'Content-Type': 'application/json' });
+
+        //         // set response content    
+
+        //         res.write(error);
+        //         res.end();
+        //     }
+
+        // }
+        // if (req.url == '/delete-cert') { //check the URL of the current request
+        //     console.log(req.body, 'abc')
+        //     try {
+        //         const child = exec(`./delete_cert.sh "*.${req.body.portalRoot}"`,
+        //             (error, stdout, stderr) => {
+
+        //                 console.log('Command:', error);
+        //                 console.log('stdout:', stdout);
+        //                 console.log('stderr:', stderr);
+
+        //                 res.writeHead(200, { 'Content-Type': 'application/json' });
+        //                 res.end(JSON.stringify({ stdout, stderr, error }));
+
+        //             });
+
+        //     } catch (error) {
+        //         res.writeHead(500, { 'Content-Type': 'application/json' });
+
+        //         // set response content    
+
+        //         res.write(error);
+        //         res.end();
+        //     }
+
+        // }
         if (req.url == '/delete-cert') { //check the URL of the current request
-            console.log(req.body, 'abc')
+            console.log(req.body, 'abc');
             try {
-                const child = exec(`./delete_cert.sh "*.${req.body.portalRoot}"`,
-                    (error, stdout, stderr) => {
+                const { stdout, stderr } = await exec(`./delete_cert.sh "*.${req.body.portalRoot}"`);
 
-                        console.log('Command:', error);
-                        console.log('stdout:', stdout);
-                        console.log('stderr:', stderr);
+                console.log('stdout:', stdout);
+                console.log('stderr:', stderr);
 
-                        res.writeHead(200, { 'Content-Type': 'application/json' });
-                        res.end(JSON.stringify({ stdout, stderr, error }));
-
-                    });
-
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ stdout, stderr, error: null }));
             } catch (error) {
+                console.log('Command error:', error);
+
                 res.writeHead(500, { 'Content-Type': 'application/json' });
-
-                // set response content    
-
-                res.write(error);
-                res.end();
+                // Assuming `error` is an Error object, you might want to stringify `error.message` instead of `error` directly
+                res.end(JSON.stringify({ error: error.message }));
             }
-
         }
+
     })
 
 
